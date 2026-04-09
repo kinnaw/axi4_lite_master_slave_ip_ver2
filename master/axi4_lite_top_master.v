f4c45608a833b2f1bdff3bf10af8ab7f094f5225
kinnaw <89127714+kinnaw@users.noreply.github.com>
2026-04-09 16:06:00 +0530

    Add axi4_lite_top_master module with parameters and ports

---
+++ master/axi4_lite_top_master.v
@@ -0,0 +1,51 @@
+module axi4_lite_top_master #(
+    parameter DATA_WIDTH = 32,
+    parameter ADDR_WIDTH = 32,
+    parameter MEM_DEPTH  = 16
+)(
+    input  wire                       ACLK,
+    input  wire                       ARESETn,
+ 
+    
+    output wire [ADDR_WIDTH-1:0]      AWADDR,
+    output wire [2:0]                 AWPROT,
+    output wire                       AWVALID,
+    input  wire                       AWREADY,   
+ 
+    
+    output wire [DATA_WIDTH-1:0]      WDATA,
+    output wire [(DATA_WIDTH/8)-1:0]  WSTRB,
+    output wire                       WVALID,
+    input  wire                       WREADY,    
+ 
+    
+    input  wire [1:0]                 BRESP,
+    input  wire                       BVALID,
+    output wire                       BREADY,    
+ 
+    
+    output wire [ADDR_WIDTH-1:0]      ARADDR,
+    output wire [2:0]                 ARPROT,
+    output wire                       ARVALID,
+    input  wire                       ARREADY,  
+ 
+   
+    input  wire [DATA_WIDTH-1:0]      RDATA,
+    input  wire [1:0]                 RRESP,
+    input  wire                       RVALID,
+    output wire                       RREADY,    
+ 
+    
+    input  wire                       wr_en,
+    input  wire [ADDR_WIDTH-1:0]      awaddr_in,
+    input  wire [DATA_WIDTH-1:0]      wdata_in,
+    input  wire [(DATA_WIDTH/8)-1:0]  wstrb_in,
+    input  wire [2:0]                 wr_prot,
+    output wire [1:0]                 bresp_out,
+ 
+    input  wire                       rd_en,
+    input  wire [ADDR_WIDTH-1:0]      araddr_in,
+    input  wire [2:0]                 rd_prot,
+    output wire [DATA_WIDTH-1:0]      data_out,
+    output wire [1:0]                 rresp_out
+);
